47ad9540482828bad88557471be0c1e8577b5079
Ngondo <j.ngondo@sap.com>
2019-03-20 14:13:49 +0000

    added more
    added more

--- JavaOOP/src/main/java/com/james/builder/pattern/BuilderRunner.java
+++ JavaOOP/src/main/java/com/james/builder/pattern/BuilderRunner.java
@@ -1,16 +1,8 @@
 package com.james.builder.pattern;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BuilderRunner {
 
 	public static void main(String[] args) {
-		
-		List<User> user = new ArrayList<User>();
-		user.add(new User.Builder("cooljay", "bbb").build());
-		user.add(new User.Builder("bob", "bbb").build());
-		user.add(new User.Builder("gilby", "bbb").build());
 		
 		User reg = new User.Builder("James","jay@yahoo.ie")
 				.phone("000000")
--- JavaOOP/src/main/java/com/training/open/closed/strategy/principle/hospitalemployee/EmergencyRoomProcessRunner.java
+++ JavaOOP/src/main/java/com/training/open/closed/strategy/principle/hospitalemployee/EmergencyRoomProcessRunner.java
@@ -5,10 +5,10 @@ public class EmergencyRoomProcessRunner {
 	public static void main(String[] args) {
 		
 		Employee cathy = new Nurse(1, "Cathy Samalani", "Emergency Dept");
-		Employee davie = new Doctor(2, "John Mponda", "Daediatric Dept");
+		Employee mponda = new Doctor(2, "John Mponda", "Daediatric Dept");
 		
-		HospitalManagement mgt = new HospitalManagement(davie);
-		mgt.callUpon(davie);
+		HospitalManagement mgt = new HospitalManagement(mponda);
+		mgt.callUpon(mponda);
 		
 
 	}
